99c1f6ede21773b57dd960d90a73af05a1d61979
Harris Hancock <harris.hancock@obscuradigital.com>
2013-01-13 15:44:31 -0800

    Clean up as_tuple code

--- as_tuple.hpp
+++ as_tuple.hpp
@@ -9,33 +9,33 @@
 #include <type_traits>
 #include <utility>
 
-#define AS_TUPLE_ref_type(R, STRUCT, ATTRIBUTE) \
+#define AS_TUPLE_ref_type(R, STRUCT, ATTRIBUTE)                              \
     typename std::add_lvalue_reference<decltype(STRUCT::ATTRIBUTE)>::type
 
-#define AS_TUPLE_ref_value(R, STRUCT_VALUE, ATTRIBUTE) \
+#define AS_TUPLE_ref_value(R, STRUCT_VALUE, ATTRIBUTE)                       \
     std::ref(STRUCT_VALUE.ATTRIBUTE)
 
-#define AS_TUPLE_cref_type(R, STRUCT, ATTRIBUTE) \
-    typename std::add_lvalue_reference< \
+#define AS_TUPLE_cref_type(R, STRUCT, ATTRIBUTE)                             \
+    typename std::add_lvalue_reference<                                      \
         typename std::add_const<decltype(STRUCT::ATTRIBUTE)>::type>::type
 
-#define AS_TUPLE_cref_value(R, STRUCT_VALUE, ATTRIBUTE) \
+#define AS_TUPLE_cref_value(R, STRUCT_VALUE, ATTRIBUTE)                      \
     std::cref(STRUCT_VALUE.ATTRIBUTE)
 
-#define AS_TUPLE_move_type(R, STRUCT, ATTRIBUTE) \
+#define AS_TUPLE_move_type(R, STRUCT, ATTRIBUTE)                             \
     decltype(STRUCT::ATTRIBUTE)
 
-#define AS_TUPLE_move_value(R, STRUCT_VALUE, ATTRIBUTE) \
+#define AS_TUPLE_move_value(R, STRUCT_VALUE, ATTRIBUTE)                      \
     std::move(STRUCT_VALUE.ATTRIBUTE)
 
-#define AS_TUPLE_def(PRE, STRUCT, POST, ATTRIBUTE_SEQ, TYPE_OP, VALUE_OP) \
-    inline std::tuple< \
-        BOOST_PP_SEQ_ENUM(BOOST_PP_SEQ_TRANSFORM(TYPE_OP, STRUCT, ATTRIBUTE_SEQ)) \
-        > \
-    as_tuple (PRE STRUCT POST s) {  \
-        return std::make_tuple( \
-            BOOST_PP_SEQ_ENUM(BOOST_PP_SEQ_TRANSFORM(VALUE_OP, s, ATTRIBUTE_SEQ)) \
-            );  \
+#define AS_TUPLE_enumerate(OP, DATA, SEQ)                                    \
+    BOOST_PP_SEQ_ENUM(BOOST_PP_SEQ_TRANSFORM(OP, DATA, SEQ))
+
+#define AS_TUPLE_def(PRE, STRUCT, POST, ATTRIBUTE_SEQ, TYPE_OP, VALUE_OP)    \
+    inline std::tuple<AS_TUPLE_enumerate(TYPE_OP, STRUCT, ATTRIBUTE_SEQ)>    \
+    as_tuple (PRE STRUCT POST s) {                                           \
+        return std::make_tuple(                                              \
+                AS_TUPLE_enumerate(VALUE_OP, s, ATTRIBUTE_SEQ));             \
     }
 
 #define AS_TUPLE_UNSAFE(STRUCT, ATTRIBUTE_SEQ) \
