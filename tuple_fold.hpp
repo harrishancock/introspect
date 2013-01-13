6b410a82172abcecf28ac2667ab84a8d3eb5e4d4
Harris Hancock <harris.hancock@obscuradigital.com>
2013-01-13 15:44:44 -0800

    Add non-const versions of folds

--- tuple_fold.hpp
+++ tuple_fold.hpp
@@ -5,6 +5,18 @@
 
 template <unsigned int N = 0, typename Func, typename Z, typename... Args>
 typename std::enable_if<sizeof...(Args) == N, Z>::type
+foldr (Func func, const Z& z, std::tuple<Args...>& t) {
+    return z;
+}
+
+template <unsigned int N = 0, typename Func, typename Z, typename... Args>
+typename std::enable_if<sizeof...(Args) != N, Z>::type
+foldr (Func func, const Z& z, std::tuple<Args...>& t) {
+    return func(std::get<N>(t), foldr<N+1>(func, z, t));
+}
+
+template <unsigned int N = 0, typename Func, typename Z, typename... Args>
+typename std::enable_if<sizeof...(Args) == N, Z>::type
 foldr (Func func, const Z& z, const std::tuple<Args...>& t) {
     return z;
 }
@@ -13,6 +25,18 @@ template <unsigned int N = 0, typename Func, typename Z, typename... Args>
 typename std::enable_if<sizeof...(Args) != N, Z>::type
 foldr (Func func, const Z& z, const std::tuple<Args...>& t) {
     return func(std::get<N>(t), foldr<N+1>(func, z, t));
+}
+
+template <unsigned int N = 0, typename Func, typename Z, typename... Args>
+typename std::enable_if<sizeof...(Args) == N, Z>::type
+foldl (Func func, const Z& z, std::tuple<Args...>& t) {
+    return z;
+}
+
+template <unsigned int N = 0, typename Func, typename Z, typename... Args>
+typename std::enable_if<sizeof...(Args) != N, Z>::type
+foldl (Func func, const Z& z, std::tuple<Args...>& t) {
+    return foldl<N+1>(func, func(z, std::get<N>(t)), t);
 }
 
 template <unsigned int N = 0, typename Func, typename Z, typename... Args>
